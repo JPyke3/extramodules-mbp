4297b827eeee5cd7d4182b4055842898df1e3109
JPyke3 <pyke.jacob1@gmail.com>
2020-07-11 20:40:44 +1000

    Added Touchbar Package to Extramodules

--- build.sh
+++ build.sh
@@ -3,30 +3,36 @@ mkdir build
 
 #bbswitch
 cd bbswitch
-makepkg -s
+makepkg -s --sign
 mv *.pkg* ../build
 rm -rf src/ pkg/
 
 #broadcom-wl
 cd ../broadcom-wl
-makepkg -s
+makepkg -s --sign
 mv *.pkg* ../build
 rm -rf src/ pkg/
 
 #virtualbox-guest-module
 cd ../virtualbox-modules
-makepkg -s
+makepkg -s --sign
 mv *.pkg* ../build
 rm -rf src/ pkg/
 
 #spl_zfs
 cd ../spl_zfs
-makepkg -s
+makepkg -s --sign
 mv *.pkg* ../build
 rm -rf src/ pkg/
 
 #apple-bce
-cd ../apple-bce-git
-makepkg -s
+cd ../apple-bce
+makepkg -s --sign
 mv *.pkg* ../build
-rm -rf src/ pkg
+rm -rf src/ pkg/
+
+#macbook12-spi-driver
+cd ../macbook12-spi-driver-dkms
+makepkg -s --sign
+mv *.pkg* ../build
+rm -rf src/ pkg/
